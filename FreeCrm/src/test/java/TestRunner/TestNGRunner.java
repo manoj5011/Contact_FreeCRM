c550306ac4e86b13fcda6e784c9bc0b89c0be955
manoj5011 <saisiddarthmanoj@gmail.com>
2021-05-06 16:07:31 +0530

    Made changes in the testNG runner path.

--- FreeCrm/src/test/java/TestRunner/TestNGRunner.java
+++ FreeCrm/src/test/java/TestRunner/TestNGRunner.java
@@ -4,7 +4,7 @@ import org.testng.annotations.DataProvider;
 
 import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
-@CucumberOptions(features ="src/test/resources/Feature/CreateContact.feature",glue={"StepDefinition"})
+@CucumberOptions(features ="src/test/resources/Feature",glue={"StepDefinition"})
 public class TestNGRunner extends AbstractTestNGCucumberTests{
  
 }

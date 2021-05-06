046e680264bd32c6506564e710b538e858b7c4b9
manoj5011 <saisiddarthmanoj@gmail.com>
2021-05-06 12:03:07 +0530

    Changes made in testng runner.

--- FreeCrm/src/test/java/TestRunner/TestNGRunner.java
+++ FreeCrm/src/test/java/TestRunner/TestNGRunner.java
@@ -1,8 +1,10 @@
 package TestRunner;
 
+import org.testng.annotations.DataProvider;
+
 import cucumber.api.CucumberOptions;
 import cucumber.api.testng.AbstractTestNGCucumberTests;
-@CucumberOptions(features ="src/test/resources/Feature/SecurityContact.feature",glue={"StepDefinition"})
+@CucumberOptions(features ="src/test/resources/Feature/CreateContact",glue={"StepDefinition"})
 public class TestNGRunner extends AbstractTestNGCucumberTests{
-
+ 
 }
